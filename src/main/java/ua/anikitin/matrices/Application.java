0e93a5bd37c91b01f60b6adab6e868318185e97d
Andrey Nikitin <nikitinandrei94@gmail.com>
2020-07-21 18:39:49 +0300

    initial commit

---
+++ src/main/java/ua/anikitin/matrices/Application.java
@@ -0,0 +1,8 @@
+package ua.anikitin.matrices;
+
+public class Application {
+
+    public static void main(String... args) {
+        System.out.println(1 % 1);
+    }
+}
---
+++ src/main/java/ua/anikitin/matrices/Matrix2DSquare.java
@@ -0,0 +1,14 @@
+package ua.anikitin.matrices;
+
+public class Matrix2DSquare {
+
+    private final int[] data;
+
+    public Matrix2DSquare(int[] data) {
+        this.data = data;
+    }
+
+    public int[] getData() {
+        return data;
+    }
+}
